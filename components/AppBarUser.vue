7e42ef10de66a362ed81415f57b2e7643fa16eb0
gerzitom <tomas.gerzicak@gmail.com>
2021-06-04 04:40:41 +0200

    Added lougout feature

--- components/AppBarUser.vue
+++ components/AppBarUser.vue
@@ -1,8 +1,18 @@
 <template>
   <div class="app-bar-user">
-    <template v-if="loggedIn">
-      <p>Tomáš Geržičák</p>
-    </template>
+    <v-menu offset-y v-if="loggedIn">
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn v-bind="attrs" v-on="on"> Tomáš Geržičák </v-btn>
+      </template>
+      <v-list>
+        <v-list-item>
+          <v-btn text>Settings</v-btn>
+        </v-list-item>
+        <v-list-item>
+          <v-btn text class="error" @click="logout">Log out</v-btn>
+        </v-list-item>
+      </v-list>
+    </v-menu>
     <v-btn to="/login" class="secondary" v-else>Log in</v-btn>
   </div>
 </template>
@@ -12,8 +22,21 @@ import { Component, Vue, Prop } from 'nuxt-property-decorator'
 
 @Component
 export default class AppBarUser extends Vue {
+  mounted() {
+    console.log(this.$auth)
+  }
   get loggedIn(): boolean {
     return this.$auth.loggedIn
+  }
+  logout() {
+    this.$auth
+      .logout()
+      .then((response) => {
+        console.log('DONE')
+      })
+      .catch((err) => {
+        console.log('ERR')
+      })
   }
 }
 </script>
