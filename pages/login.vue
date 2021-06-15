cc250c940e530a52db97662dd433b3705a305947
gerzitom <tomas.gerzicak@gmail.com>
2021-06-15 21:50:31 +0200

    Login process refactored

--- pages/login.vue
+++ pages/login.vue
@@ -2,7 +2,7 @@
   <div class="login-page">
     <v-card max-width="400" class="pa-5 mx-auto mt-10">
       <h1 class="text-h2 mb-3">Login</h1>
-      <v-form onsubmit="return false" @submit="userLogin">
+      <v-form onsubmit="return false" @submit="userLogin" ref="form">
         <v-select
           v-model="selectedUser"
           :items="availableUsers"
@@ -13,11 +13,19 @@
           outlined
         ></v-select>
         <template v-if="!selectedUser">
-          <v-text-field v-model="userId" label="User ID" :disabled="loading" />
+          <v-text-field
+            v-model="userId"
+            label="User ID"
+            :disabled="loading"
+            :error="tenantIdError.isError"
+            :error-messages="tenantIdError.messages"
+            :rules="[userIdRequired]"
+          />
           <v-text-field
             v-model="login.username"
             label="Username"
             :disabled="loading"
+            :rules="[usernameRequired]"
           />
         </template>
         <v-text-field
@@ -27,6 +35,9 @@
           :disabled="loading"
           name="input-10-1"
           label="Password"
+          :error="passwordError.isError"
+          :error-messages="passwordError.messages"
+          :rules="[passwordRequired]"
           @click:append="show = !show"
         ></v-text-field>
 
@@ -47,6 +58,8 @@
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator'
 import LocalUser from '@/types/LocalUser'
+import User from '~/types/User'
+import { AxiosError } from 'axios'
 
 @Component
 export default class LoginPage extends Vue {
@@ -55,6 +68,9 @@ export default class LoginPage extends Vue {
   selectedUser: LocalUser | null = null
   show = false
   loading = false
+
+  tenantIdError: ErrorData = new ErrorData()
+  passwordError: ErrorData = new ErrorData()
 
   /**
    * Getting available users from local storage.
@@ -81,40 +97,102 @@ export default class LoginPage extends Vue {
    * Global error is dispatched, if API request failed
    */
   async userLogin() {
+    if (!this.validateInputs()) return
+    this.resetErrors()
     this.loading = true
     if (this.selectedUser) {
       this.login.username = this.selectedUser.username
       this.userId = this.selectedUser.userId
     }
-    // TODO Bug after refreshing page
-    const userInfo = await this.$store.dispatch('user/getUserInfo', this.userId)
-    await this.$auth.$storage.setUniversal('apiKey', userInfo.apiKey)
-    this.$axios.defaults.headers.common['X-API-KEY'] = userInfo.apiKey
-    this.$axios.setHeader('X-API-KEY', userInfo.apiKey)
-    this.$auth
-      .loginWith('local', { data: this.login })
-      .then(() => {
-        const user = new LocalUser(this.userId, this.login.username)
-        this.$auth.setUser(userInfo)
-        this.$auth.$storage.setUniversal('user', userInfo)
-        this.addUserToLocalStorage(user)
+    try {
+      const userInfo = await this.$store.dispatch(
+        'user/getUserInfo',
+        this.userId
+      )
+      await this.storeApikey(userInfo.apiKey)
+
+      await this.$auth.loginWith('local', { data: this.login })
+      await this.setAuthUser(userInfo)
+      const user = new LocalUser(this.userId, this.login.username)
+      this.saveUserLoginInfo(user)
+      this.loading = false
+    } catch (err) {
+      let errorTitle = this.getErrorMessage(err)
+      this.$nuxt.$emit('temporary-error', {
+        message: errorTitle,
       })
-      .catch((err) => {
-        this.$nuxt.$emit('error', {
-          title: 'API error',
-          message: err.message,
-        })
-      })
-      .finally(() => {
-        this.loading = false
-      })
+      this.loading = false
+    }
   }
 
   /**
-   * Adds user and api key to local storage
+   * Basic validation of form if every field is not empty
+   */
+  validateInputs(): boolean {
+    ;(this.$refs.form as Vue & { validate: () => boolean }).validate()
+    if (this.selectedUser && this.login.password !== '') return true
+    else
+      return (
+        this.userId !== '' &&
+        this.login.username !== '' &&
+        this.login.password !== ''
+      )
+  }
+
+  /**
+   * Gets error message for global error snackbar
+   * If error message supports multiple errors object,
+   * add error messages right as input error messages to vuetify component.
+   * For more info: https://vuetifyjs.com/en/api/v-text-field/#props-error
+   */
+  getErrorMessage(error: AxiosError): string {
+    let errMessage = ''
+    if (error.response && error.response.data) {
+      const errorsObj = error.response.data.errors
+      if (errorsObj) {
+        for (const err in errorsObj) {
+          if (err === 'tenantId') {
+            this.tenantIdError.isError = true
+            this.tenantIdError.messages = errorsObj[err]
+          } else if (err === 'Password') {
+            this.passwordError.isError = true
+            this.passwordError.messages = errorsObj[err]
+          }
+        }
+        if (error.response) errMessage = error.response.data.title
+      } else {
+        if (error.response) errMessage = error.response.data.message
+      }
+    }
+    return errMessage
+  }
+
+  /**
+   * Stores API key into local storage for axios plugin
+   * Sets API key to axios for login call
+   */
+  async storeApikey(apiKey: string) {
+    await this.$auth.$storage.setUniversal('apiKey', apiKey)
+    this.$axios.defaults.headers.common['X-API-KEY'] = apiKey
+    this.$axios.setHeader('X-API-KEY', apiKey)
+  }
+
+  /**
+   * Sets user to auth plugin
+   * Save user to local storage
+   */
+  async setAuthUser(userInfo: User) {
+    this.$auth.setUser(userInfo)
+    await this.$auth.$storage.setUniversal('user', userInfo)
+  }
+
+  /**
+   * Save user ID and username to be used for future logins.
+   * In future logins user do not have to type this credentials,
+   * only choose right credentials
    * @param user to be added to local storage
    */
-  addUserToLocalStorage(user: LocalUser) {
+  saveUserLoginInfo(user: LocalUser) {
     let users = this.$auth.$storage.getLocalStorage('users') as LocalUser[]
     if (!users) {
       users = []
@@ -127,6 +205,29 @@ export default class LoginPage extends Vue {
       this.$auth.$storage.setLocalStorage('users', users)
     }
   }
+
+  resetErrors() {
+    this.tenantIdError = new ErrorData()
+    this.passwordError = new ErrorData()
+  }
+
+  //rules
+  passwordRequired(value: any): any {
+    return !!value || 'Password required.'
+  }
+
+  usernameRequired(value: any): any {
+    return !!value || 'Username required.'
+  }
+
+  userIdRequired(value: any): any {
+    return !!value || 'User ID required.'
+  }
+}
+
+class ErrorData {
+  isError: boolean = false
+  messages: string[] = []
 }
 
 /**
